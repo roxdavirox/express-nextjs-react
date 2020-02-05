bae490148667f91abb8846dc9387f5b8477bf6e7
roxdavirox <roxdavirox@gmail.com>
2020-02-05 17:14:35 -0300

    new form

---
+++ .next/server/static/development/pages/form.js
@@ -0,0 +1,181 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../../../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/pages/form.js":
+/*!***************************!*\
+  !*** ./src/pages/form.js ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+const React = __webpack_require__(/*! react */ "react");
+
+var __jsx = React.createElement;
+
+const fetch = __webpack_require__(/*! isomorphic-fetch */ "isomorphic-fetch");
+
+const Form = ({
+  product,
+  options
+}) => {
+  return __jsx(React.Fragment, null, __jsx("div", null, "Produto: ", product.name, " "), __jsx("ul", null, Object.keys(options).map(k => __jsx("li", {
+    key: options[k]._id
+  }, options[k].name, __jsx("ul", null, options[k].items.map(item => __jsx("li", {
+    key: item._id
+  }, item.name)))))));
+};
+
+Form.getInitialProps = async ({
+  req,
+  res,
+  query
+}) => {
+  try {
+    const response = await fetch(`https://mktp.herokuapp.com/form/${query.id}`);
+    const data = await response.json();
+    console.log('data', data);
+    const {
+      product,
+      options
+    } = data;
+    return {
+      product,
+      options
+    };
+  } catch (e) {
+    console.log('error ====> : ', e);
+  }
+};
+
+module.exports = Form;
+
+/***/ }),
+
+/***/ 4:
+/*!*********************************!*\
+  !*** multi ./src/pages/form.js ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\www\express-nextjs-react\src\pages\form.js */"./src/pages/form.js");
+
+
+/***/ }),
+
+/***/ "isomorphic-fetch":
+/*!***********************************!*\
+  !*** external "isomorphic-fetch" ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("isomorphic-fetch");
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=form.js.map
--- src/pages/form.js
+++ src/pages/form.js
@@ -1,9 +1,36 @@
 const React = require("react");
+const fetch = require("isomorphic-fetch");
 
-const Form = ({ msg }) => <div>Ola mundo {msg}</div>;
+const Form = ({ product, options }) => {
+
+  return (
+    <>
+      <div>Produto: {product.name} </div>
+      <ul>
+        {Object.keys(options).map(k => 
+          <li key={options[k]._id}>{options[k].name}
+            <ul>
+              {options[k].items.map(item => <li key={item._id}>{item.name}</li>)}
+            </ul>
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}
 
 Form.getInitialProps = async ({ req, res, query }) => {
-  return { msg: req.body.msg };
+  try {
+    const response = await fetch(
+      `https://mktp.herokuapp.com/form/${query.id}`
+    );
+    const data = await response.json();
+    console.log('data', data);
+    const { product, options } = data;
+    return { product, options };
+  } catch (e) {
+    console.log('error ====> : ', e);
+  }
 };
 
 module.exports = Form;
